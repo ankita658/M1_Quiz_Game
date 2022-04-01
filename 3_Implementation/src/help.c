ac2af6e7d6092564503bdfa8e38c33ed9c3eba73
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-04-01 16:55:27 +0530

    Create help.c

---
+++ 3_Implementation/src/help.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<time.h>
+#include<direct.h>
+#include"main.h"
+
+void need_help()
+ {
+ 
+ system("cls");
+ printf("\n\n\n\tThis game is very easy to play. You'll be asked some general");
+ printf("\n\n\tknowledge questions and the right answer is to be chosen among");
+ printf("\n\n\tthe four options provided. Your score will be calculated at the");
+ printf("\n\n\tend. Remember that the more quicker you give answer the more");
+ printf("\n\n\tscore you will secure. Your score will be calculated and displayed");
+ printf("\n\n\tat the end and displayed. If you secure highest score, your score");
+ printf("\n\n\twill be recorded. So BEST OF LUCK.");
+ 
+ }
