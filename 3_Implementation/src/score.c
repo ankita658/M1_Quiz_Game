86f52264e97c488c8fe254d6e15eca5fa8e67982
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-04-01 16:51:08 +0530

    Create score.c

---
+++ 3_Implementation/src/score.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<time.h>
+#include<direct.h>
+#include"main.h"
+
+
+void score_display()
+ {
+ char name[20];
+ float s;
+ FILE *f;
+ system("cls");
+ f=fopen("points.txt","r");
+ fscanf(f,"%s%f",&name,&s);
+ printf("\n\n\t\t ");
+ printf("\n\n\t\t %s has secured the Highest points %.2f",name,s);
+ printf("\n\n\t\t ");
+ fclose(f);
+ getch();
+ }
