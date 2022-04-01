edd1ed74a152b805606acea41fe16fd27733203a
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-04-01 16:53:29 +0530

    Update display.c

--- display.c
+++ display.c
@@ -10,15 +10,19 @@
 
 void score_display()
  {
+ 
  char name[20];
  float s;
  FILE *f;
+ 
  system("cls");
  f=fopen("points.txt","r");
  fscanf(f,"%s%f",&name,&s);
+ 
  printf("\n\n\t\t ");
  printf("\n\n\t\t %s has secured the Highest points %.2f",name,s);
  printf("\n\n\t\t ");
+ 
  fclose(f);
  getch();
  }
