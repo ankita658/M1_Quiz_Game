5ad41c7df71737033221147df69596c1cab48e5a
ankita658 <81344818+ankita658@users.noreply.github.com>
2022-04-03 20:22:47 +0530

    Update score.c

--- 3_Implementation/src/score.c
+++ 3_Implementation/src/score.c
@@ -9,17 +9,22 @@
 
 void type_score(float points,char plnm[20])
  {
+ 
  float sc;
  char nm[20];
  FILE *f;
+ 
  system("cls");
  f=fopen("points.txt","r");
  fscanf(f,"%s%f",&nm,&sc);
+ 
  if (points>=sc)
-   { sc=points;
+   {
+     sc=points;
      fclose(f);
      f=fopen("points.txt","w");
      fprintf(f,"%s\n%.2f",plnm,sc);
      fclose(f);
+    
    }
  }
